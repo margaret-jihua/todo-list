6267cf0ba4d7c6d8dd01f9b8503b79f604e0f179
margare-jihua <margaret.jihua@gmail.com>
2020-08-26 15:32:50 -0700

    [Add Item] state manipulation

--- src/MyList.js
+++ src/MyList.js
@@ -3,9 +3,38 @@ import './App.css';
 import ListItem from './ListItem'
 
 class MyList extends Component {
-  
+
+  constructor (props) {
+    super()
+    this.state = {
+      toDoItemArray: props.theList,
+      newItem: ''
+    }
+  }
+
+  newItemChange(e) {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault()
+    let array = this.state.toDoItemArray
+    array.push(this.state.newItem)
+    this.setState({
+      toDoItemArray: array
+    })
+  }
+
+  clearList (e) {
+    this.setState({
+      toDoItemArray: []
+    })
+  }
+
   render() {
-    let todoItems = this.props.theList.map((item, index) => (
+    let todoItems = this.state.toDoItemArray.map((item, index) => (
       <ListItem doThis={item} key={index} />
     ))
     return (
@@ -13,7 +42,16 @@ class MyList extends Component {
       <h1> Things I should stop procrastinating:</h1>
       <ul>
         {todoItems}
-      </ul>        
+      </ul>  
+      <form>
+        <input type="text"
+        placeholder="Type an item here"
+        onChange={(e) => this.newItemChange(e)}
+        value={this.state.newItem}
+        />
+        <button onClick={(e) => this.addItem(e)}>Add it!</button>
+      </form>
+      <button onClick={(e) => this.clearList(e)}>Finished the list!</button>      
     </div>
     );
   }
